338c5494ac82be926149cb3743d7fca72d7f988b
Harshil Goyal <arveharshil@gmail.com>
2026-06-11 16:21:20 +0530

    Add nexus.bat launcher

--- install.ps1
+++ install.ps1
@@ -11,3 +11,7 @@ git clone https://github.com/john123682-2/nexus.git $folder
 Write-Host ""
 Write-Host "NEXUS Installed!"
 Write-Host "Location: $folder"
+Write-Host ""
+Write-Host "Launching NEXUS..."
+
+python "$env:USERPROFILE\nexus\main.py"
